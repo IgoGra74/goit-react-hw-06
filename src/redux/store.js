a32e720acc8723e1876810412691798b7900b4ac
IgoGra74 <142592937+IgoGra74@users.noreply.github.com>
2024-04-10 13:27:41 +0300

    update

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// // import { rootReducer } from "./reducer";
+
+// const store = configureStore({
+//   reducer: {
+//     contacts: {
+//       items: [],
+//     },
+//     filters: {
+//       name: "",
+//     },
+//   },
+// });
+// export default store;
